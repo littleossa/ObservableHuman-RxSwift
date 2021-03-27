a151ae0abdc81afd585af687e6bef780ae980855
Lil Ossa <littleossa@gmail.com>
2021-03-27 18:13:35 +0900

    Linked each outlet with ObservableHumanWithUI.ViewController

--- ObservableHuman-RxSwift/ObservableHumanWithUI/ObservableHumanWithUIViewController.swift
+++ ObservableHuman-RxSwift/ObservableHumanWithUI/ObservableHumanWithUIViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ObservableHumanWithUIViewController: UIViewController {
-
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var clothesControl: UISegmentedControl!
+    @IBOutlet weak var footwearControl: UISegmentedControl!
+    @IBOutlet weak var humanImage: UIImageView!
+    @IBOutlet weak var underAgeGateView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        underAgeGateView.isHidden = true
     }
     
 
